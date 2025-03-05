fd1da2838b3efad08b41bbe4d1e213101741194d
Omar <uksaid12@gmail.com>
2025-03-05 13:37:27 +0100

    added shap and nearmiss

--- src/dependencies.py
+++ src/dependencies.py
@@ -26,6 +26,10 @@ from sklearn.feature_selection import mutual_info_classif
 from imblearn.over_sampling import SMOTE
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import PolynomialFeatures, OneHotEncoder
+import benford as bf
+from sklearn.model_selection import StratifiedShuffleSplit
+from imblearn.pipeline import make_pipeline
+from imblearn.under_sampling import NearMiss
 
 # Machine Learning Algorithms
 from sklearn.svm import SVC, LinearSVC
@@ -33,6 +37,23 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.cluster import KMeans
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.ensemble import StackingClassifier
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, ExtraTreesClassifier, HistGradientBoostingClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import NuSVC
+from sklearn.neighbors import RadiusNeighborsClassifier
+
+# Outlier Detection
 import xgboost as xgb
 from sklearn.decomposition import PCA
 from sklearn.neighbors import LocalOutlierFactor
@@ -58,8 +79,22 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, explained_variance_score
 from sklearn.metrics import roc_curve, roc_auc_score
 from sklearn.metrics import precision_recall_curve, average_precision_score
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+
+# Hyperparameter Tuning
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+
+# Time Series Analysis
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from pmdarima.arima import auto_arima
 
 # Miscellaneous
+from collections import Counter
 import itertools
 from functools import reduce
 import matplotlib.patches as mpatches
